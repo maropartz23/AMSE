3eb1a6472e2ba890fba171c357fca47a7fed85a7
Maropartz <martin.ropartz@etu.imt-nord-europe.fr>
2025-03-03 14:17:13 +0100

    classification et exo 4

---
+++ taquin/lib/exo2.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
+
+class Exo2 extends StatefulWidget {
+  @override
+  _Exo2State createState() => _Exo2State();
+}
+
+class _Exo2State extends State<Exo2> {
+  double _currentRotateXValue = 0;
+  double _currentRotateYValue = 0;
+  double _currentScaleValue = 1.0;
+  bool miroir = false;
+  bool isAnimating = false;
+  Timer? _timer;
+
+  void startAnimation() {
+    if (_timer != null && _timer!.isActive) return;
+
+    _timer = Timer.periodic(Duration(milliseconds: 50), (timer) {
+      setState(() {
+        _currentRotateXValue = (_currentRotateXValue + 2) % 360;
+        _currentRotateYValue = (_currentRotateYValue + 1) % 360;
+      });
+    });
+  }
+
+  void stopAnimation() {
+    _timer?.cancel();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Exo 2 - Animation & Transform')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10),
+
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateX(_currentRotateXValue * pi / 180)
+                  ..rotateY(_currentRotateYValue * pi / 180)
+                  ..scale(miroir ? -_currentScaleValue : _currentScaleValue),
+                child: Image.network(
+                  'https://picsum.photos/512/1024',
+                  width: 400,
+                  height: 400,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            // Rotate X
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Rotate X :',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Slider(
+                  value: _currentRotateXValue,
+                  min: 0,
+                  max: 360,
+                  onChanged: (double value) {
+                    setState(() {
+                      _currentRotateXValue = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Rotate Y :',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Slider(
+                  value: _currentRotateYValue,
+                  min: 0,
+                  max: 360,
+                  onChanged: (double value) {
+                    setState(() {
+                      _currentRotateYValue = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Scale :',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Slider(
+                  value: _currentScaleValue,
+                  min: 0.5,
+                  max: 2.0,
+                  onChanged: (double value) {
+                    setState(() {
+                      _currentScaleValue = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+
+            SwitchListTile(
+              value: miroir,
+              title: Text(
+                "Effet Miroir",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              onChanged: (bool value) {
+                setState(() {
+                  miroir = value;
+                });
+              },
+            ),
+
+            SwitchListTile(
+              value: isAnimating,
+              title: Text(
+                "Activer Animation",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              onChanged: (bool value) {
+                setState(() {
+                  isAnimating = value;
+                  if (isAnimating) {
+                    startAnimation();
+                  } else {
+                    stopAnimation();
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'dart:async';
+import 'exo1.dart';
+import 'exo2.dart';
+import 'exo4.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,127 +11,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ImageTransformPage(),
+      title: 'TP Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MenuPage(),
     );
   }
 }
 
-class ImageTransformPage extends StatefulWidget {
-  @override
-  _ImageTransformPageState createState() => _ImageTransformPageState();
-}
-
-class _ImageTransformPageState extends State<ImageTransformPage> {
-  double _rotationZ = 0.0;
-  double _rotationX = 0.0;
-  double _rotationY = 0.0;
-  double _scale = 1.0;
-  bool _isMirrored = false;
-  late Timer _timer;
-  bool _isAnimating = false;
-
-  void _startAnimation() {
-    _isAnimating = true;
-    _timer = Timer.periodic(Duration(milliseconds: 50), (timer) {
-      setState(() {
-        _rotationZ < pi - 0.06 ? _rotationZ += 0.05 : _rotationZ = -pi;
-        _rotationX < pi - 0.04 ? _rotationX += 0.03 : _rotationX = -pi;
-        _rotationY < pi - 0.03 ? _rotationY += 0.02 : _rotationY = -pi;
-      });
-    });
-  }
-
-  void _stopAnimation() {
-    _isAnimating = false;
-    _timer.cancel();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
+class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Transformations d'Image")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: Text('Menu')),
+      body: ListView(
         children: [
-          Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()
-              ..rotateX(_rotationX)
-              ..rotateY(_rotationY)
-              ..rotateZ(_rotationZ)
-              ..scale(_isMirrored ? -_scale : _scale, _scale),
-            child: Image.network(
-              'https://picsum.photos/300/300',
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 1'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Exo1()),
+                );
+              },
             ),
           ),
-          SizedBox(height: 20),
-          Text("Rotation Z"),
-          Slider(
-            value: _rotationZ,
-            min: -pi,
-            max: pi,
-            onChanged: (value) {
-              setState(() {
-                _rotationZ = value;
-              });
-            },
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 2'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Exo2()),
+                );
+              },
+            ),
           ),
-          Text("Rotation X"),
-          Slider(
-            value: _rotationX,
-            min: -pi,
-            max: pi,
-            onChanged: (value) {
-              setState(() {
-                _rotationX = value;
-              });
-            },
-          ),
-          Text("Rotation Y"),
-          Slider(
-            value: _rotationY,
-            min: -pi,
-            max: pi,
-            onChanged: (value) {
-              setState(() {
-                _rotationY = value;
-              });
-            },
-          ),
-          Text("Taille"),
-          Slider(
-            value: _scale,
-            min: 0.5,
-            max: 2.0,
-            onChanged: (value) {
-              setState(() {
-                _scale = value;
-              });
-            },
-          ),
-          SwitchListTile(
-            title: Text("Effet miroir"),
-            value: _isMirrored,
-            onChanged: (value) {
-              setState(() {
-                _isMirrored = value;
-              });
-            },
-          ),
-          ElevatedButton(
-            onPressed: _isAnimating ? _stopAnimation : _startAnimation,
-            child: Text(
-                _isAnimating ? "Arrêter l'animation" : "Démarrer l'animation"),
+          Card(
+            elevation: 5,
+            child: ListTile(
+              title: Text('Exercice 4'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DisplayTileWidget()),
+                );
+              },
+            ),
           ),
         ],
       ),
